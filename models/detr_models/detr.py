65430571a2e1d24ad0d938972d44b9ba87fb72af
Klemen Kotar <klemenk@ip-10-0-0-21.us-west-2.compute.internal>
2022-04-03 11:13:29 -0700

    Added multi step update run

--- models/detr_models/detr.py
+++ models/detr_models/detr.py
@@ -119,7 +119,7 @@ class SetCriterion(nn.Module):
 
         weight = torch.ones(1236, device=src_logits.device)
         weight[-1] *= 10.0
-        loss_ce = F.cross_entropy(src_logits.transpose(1, 2), target_classes, weight)
+        loss_ce = F.cross_entropy(src_logits.transpose(1, 2), target_classes)
         losses = {'loss_ce': loss_ce}
 
         if log:
