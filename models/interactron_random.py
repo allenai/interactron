266271cfbd98582831225d4e5e083403f8c0d769
Klemen Kotar <klemenk@ip-10-0-0-21.us-west-2.compute.internal>
2022-04-14 10:04:06 -0700

    Added runn with a little bit cheating

--- models/interactron_random.py
+++ models/interactron_random.py
@@ -125,7 +125,7 @@ class interactron_random(nn.Module):
             set_parameters(self.detector, fast_weights)
 
             post_adaptive_out = self.detector(NestedTensor(img[task], mask[task]))
-            supervisor_loss = self.criterion(post_adaptive_out, labels[task][1:], background_c=0.1)
+            supervisor_loss = self.criterion(post_adaptive_out, labels[task], background_c=0.1)
             supervisor_losses.append({k: v.detach() for k, v in supervisor_loss.items()})
             supervisor_loss = supervisor_loss["loss_ce"] + 5 * supervisor_loss["loss_giou"] + \
                               2 * supervisor_loss["loss_bbox"]
