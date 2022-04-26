b7de06fbb4960e078e6801b34c3011f2447616f6
Klemen Kotar <klemenk@ip-10-0-0-21.us-west-2.compute.internal>
2022-04-26 13:35:20 -0700

    Added interactron evaluate run

--- models/transformer.py
+++ models/transformer.py
@@ -37,7 +37,7 @@ class Transformer(nn.Module):
         # self.img_feature_embedding = nn.Linear(config.IMG_FEATURE_SIZE, config.EMBEDDING_DIM)
         # self.prediction_embedding = nn.Linear(config.NUM_CLASSES + 5, config.EMBEDDING_DIM)
         self.prediction_embedding = nn.Linear(config.BOX_EMB_SIZE + config.NUM_CLASSES + 5, config.EMBEDDING_DIM)
-        self.prediction_embedding = nn.Linear(config.BOX_EMB_SIZE, config.EMBEDDING_DIM)
+        # self.prediction_embedding = nn.Linear(config.BOX_EMB_SIZE, config.EMBEDDING_DIM)
         self.model = GPT(config)
         self.box_decoder = MLP(config.OUTPUT_SIZE, 256, 4, 3)
         self.logit_decoder = nn.Linear(config.OUTPUT_SIZE, config.NUM_CLASSES + 1)
