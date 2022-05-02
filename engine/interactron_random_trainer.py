979ca1fa3e282a280572b373a64508cea42d1ea7
Klemen Kotar <klemenk@ip-10-0-0-21.us-west-2.compute.internal>
2022-05-01 23:43:50 -0700

    Added run with adam for the detector and no weight decoy for the superviosr

--- engine/interactron_random_trainer.py
+++ engine/interactron_random_trainer.py
@@ -52,8 +52,8 @@ class InteractronRandomTrainer:
     def train(self):
         model, config = self.model, self.config.TRAINER
         raw_model = self.model.module if hasattr(self.model, "module") else self.model
-        detector_optimizer = torch.optim.AdamW(raw_model.detector.parameters(), lr=1e-5)
-        supervisor_optimizer = torch.optim.AdamW(raw_model.fusion.parameters(), lr=1e-4, weight_decay=1e-4)
+        detector_optimizer = torch.optim.Adam(raw_model.detector.parameters(), lr=1e-5)
+        supervisor_optimizer = torch.optim.AdamW(raw_model.fusion.parameters(), lr=1e-4)
         model.train()
 
         def run_epoch(split):
